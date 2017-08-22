ae0c871454c1345ca726f2ef09b717d328416909
Mattias Petter Johansson <mpj@mpj.me>
2017-08-22 13:17:12 +0200

    Filter also applies to ALL the dataz

--- src/App.js
+++ src/App.js
@@ -113,7 +113,11 @@ class App extends Component {
     }, 1000);
   }
   render() {
-
+    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+      .filter(playlist =>
+        playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+    ) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -121,15 +125,12 @@ class App extends Component {
           <h1 style={{...defaultStyle, 'font-size': '54px'}}>
             {this.state.serverData.user.name}'s Playlists
           </h1>
-          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <PlaylistCounter playlists={playlistToRender}/>
+          <HoursCounter playlists={playlistToRender}/>
           <Filter onTextChange={text => {
               this.setState({filterString: text})
             }}/>
-          {this.state.serverData.user.playlists.filter(playlist =>
-            playlist.name.toLowerCase().includes(
-              this.state.filterString.toLowerCase())
-          ).map(playlist => 
+          {playlistToRender.map(playlist => 
             <Playlist playlist={playlist} />
           )}
         </div> : <h1 style={defaultStyle}>Loading...</h1>
