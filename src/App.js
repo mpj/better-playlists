efc5c4879b2ffa5aab5b229993dc3c5869d8474f
Mattias Petter Johansson <mpj@mpj.me>
2017-08-22 18:24:01 +0200

    Added check for if we have an access token

--- src/App.js
+++ src/App.js
@@ -87,7 +87,8 @@ class App extends Component {
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
-
+    if (!accessToken)
+      return;
     fetch('https://api.spotify.com/v1/me', {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
