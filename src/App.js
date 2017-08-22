e3fff60805c4102f7378f83969255bec100036e9
Mattias Petter Johansson <mpj@mpj.me>
2017-08-22 18:31:46 +0200

    WE SUCK!

--- src/App.js
+++ src/App.js
@@ -137,7 +137,7 @@ class App extends Component {
             <Playlist playlist={playlist} />
           )}
         </div> : <button onClick={() => {
-            window.location = window.location.includes('localhost') 
+            window.location = window.location.href.includes('localhost') 
               ? 'http://localhost:8888/login' 
               : 'https://better-playlists.herokuapp.com/login' }
           }
